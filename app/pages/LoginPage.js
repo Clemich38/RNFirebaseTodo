51a9cc2b7a1479d645d1d7e61df5bc62acb0e5be
Clemich38 <clement.letreguilly@gmail.com>
2017-04-24 18:58:07 +0900

    add firebase connection

--- app/pages/LoginPage.js
+++ app/pages/LoginPage.js
@@ -4,9 +4,48 @@ import {
   View,
   Button,
   StyleSheet,
+  TextInput,
 } from 'react-native';
+import * as firebase from "firebase";
 
 export default class LoginPage extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName: "",
+      pwd: ""
+    }
+    this.initFireBase();
+  }
+
+  initFireBase(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyBHOWbyrf6Mhfmz-oSpI6dDmU9v4AeJt_o",
+      authDomain: "rn-todo-app-ba94c.firebaseapp.com",
+      databaseURL: "https://rn-todo-app-ba94c.firebaseio.com",
+      projectId: "rn-todo-app-ba94c",
+      storageBucket: "rn-todo-app-ba94c.appspot.com",
+      messagingSenderId: "1043444153224"
+    });
+  }
+
+  async signup(email, pass) {
+    const { navigate } = this.props.navigation;
+
+    try {
+      await firebase.auth()
+        .createUserWithEmailAndPassword(email, pass);
+
+      console.log("Account created");
+
+      // Navigate to the Home page, the user is auto logged in
+
+    } catch (error) {
+      console.log(error.toString())
+    }
+
+  }
 
   static navigationOptions = {
     title: 'Login',
@@ -18,6 +57,19 @@ export default class LoginPage extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Login Page</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(userName) => this.setState({ userName })}
+          value={this.state.userName}
+          placeholder="email address"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={(pwd) => this.setState({ pwd })}
+          value={this.state.pwd}
+          placeholder="password"
+          secureTextEntry={true}
+        />
         <Button
           onPress={() => navigate('Todo')}
           color={'#484848'}
@@ -41,4 +93,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center'
   },
+  input: {
+    height: 40,
+    backgroundColor: '#E0E0E0',
+    borderWidth: 0,
+    marginTop: 5,
+    marginBottom: 5,
+  }
 })
